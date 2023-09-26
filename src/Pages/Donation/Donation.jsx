6d39c9dc66386cfdda6060150c5c2dc2214e2295
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-09-26 09:17:07 +0600

    Make Donation Page Logic stronger

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -15,36 +15,44 @@ const Donation = () => {
     fetchAllDonation();
     const donations = JSON.parse(localStorage.getItem('card'));
     setDonation(donations);
-    const displayCard = donation.map((id) => allDonation[id - 1]);
-    setDisplayDonation(displayCard);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [allDonation]);
+  }, []); // Empty dependency array to run this effect only once
 
-  // console.log(displayDonation);
-
+  useEffect(() => {
+    if (donation && allDonation.length > 0) {
+      const displayCard = donation.map((id) => allDonation[id - 1]);
+      setDisplayDonation(displayCard);
+    } else {
+      setDisplayDonation([]);
+    }
+  }, [donation, allDonation]);
+  console.log('yes i got it');
   return (
     <>
       <div className="mt-11 mb-32">
-        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 mb-10">
-          {isShowAll
-            ? displayDonation.map((donation) => (
-                <DonationCard
-                  key={donation.id}
-                  donation={donation}
-                ></DonationCard>
-              ))
-            : displayDonation
-                .slice(0, 4)
-                .map((donation) => (
+        {donation ? (
+          <div className="grid grid-cols-1 gap-5 md:grid-cols-2 mb-10">
+            {isShowAll
+              ? displayDonation.map((donation) => (
                   <DonationCard
                     key={donation.id}
                     donation={donation}
                   ></DonationCard>
-                ))}
-        </div>
+                ))
+              : displayDonation
+                  .slice(0, 4)
+                  .map((donation) => (
+                    <DonationCard
+                      key={donation.id}
+                      donation={donation}
+                    ></DonationCard>
+                  ))}
+          </div>
+        ) : (
+          <div>Error</div>
+        )}
         {/* Show all */}
         <div className="text-center">
-          {isShowAll || (
+          {displayDonation.length > 4 && (
             <button
               className="font-semibold w-fit px-3 py-2 bg-[#009444] text-white rounded text-center"
               onClick={() => setIsShowAll(!isShowAll)}
