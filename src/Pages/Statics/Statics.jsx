14af1b88b627ca9086cf5d7c37796b411c835877
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-09-26 09:38:39 +0600

    if donate is empty then static page design

--- src/Pages/Statics/Statics.jsx
+++ src/Pages/Statics/Statics.jsx
@@ -1,54 +1,69 @@
 import { useLoaderData } from 'react-router-dom';
 import CanvasJSReact from '@canvasjs/react-charts';
+import ErrorPage from '../Error/ErrorPage';
 
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 const Statics = () => {
   const allCards = useLoaderData();
-  const allDonationAmount = allCards.map((card) => card.donation);
-  // console.log(allDonationAmount);
-  const totalDonationAmount = allDonationAmount.reduce(
-    (accumulator, currentValue) => accumulator + currentValue
-  );
-  const donatedCardId = JSON.parse(localStorage.getItem('card'));
-  const donatedCard = donatedCardId.map((id) => allCards[id - 1]);
-  const paidDonationAmount = donatedCard.map((card) => card.donation);
-  const totalPaidDonationAmount = paidDonationAmount.reduce(
-    (accumulator, currentValue) => accumulator + currentValue
-  );
-  // console.log(totalDonationAmount, totalPaidDonationAmount);
-  // Percentage Calculation:
-  const donationPercentage = (
-    (totalPaidDonationAmount / totalDonationAmount) *
-    100
-  ).toFixed(2);
-  const percentageRemaining = (100 - donationPercentage).toFixed(2);
+  if (allCards.length) {
+    const allDonationAmount = allCards.map((card) => card.donation);
+    // console.log(allDonationAmount);
+    const totalDonationAmount = allDonationAmount.reduce(
+      (accumulator, currentValue) => accumulator + currentValue
+    );
+    const donatedCardId = JSON.parse(localStorage.getItem('card'));
+    if (donatedCardId) {
+      const donatedCard = donatedCardId.map((id) => allCards[id - 1]);
+      const paidDonationAmount = donatedCard.map((card) => card.donation);
+      const totalPaidDonationAmount = paidDonationAmount.reduce(
+        (accumulator, currentValue) => accumulator + currentValue
+      );
+      // console.log(totalDonationAmount, totalPaidDonationAmount);
+      // Percentage Calculation:
+      const donationPercentage = (
+        (totalPaidDonationAmount / totalDonationAmount) *
+        100
+      ).toFixed(2);
+      const percentageRemaining = (100 - donationPercentage).toFixed(2);
 
-  //Create Chart {
-  const options = {
-    animationEnabled: true,
-    exportFileName: 'Donation Status',
-    exportEnabled: false,
-    data: [
-      {
-        type: 'pie',
-        showInLegend: true,
-        legendText: '{label}',
-        toolTipContent: '{label}: <strong>{y}%</strong>',
-        indexLabel: '{y}%',
-        indexLabelPlacement: 'inside',
-        dataPoints: [
-          { y: percentageRemaining, label: 'Total Donation' },
-          { y: donationPercentage, label: 'Your Donation' },
+      //Create Chart {
+      const options = {
+        animationEnabled: true,
+        exportFileName: 'Donation Status',
+        exportEnabled: false,
+        data: [
+          {
+            type: 'pie',
+            showInLegend: true,
+            legendText: '{label}',
+            toolTipContent: '{label}: <strong>{y}%</strong>',
+            indexLabel: '{y}%',
+            indexLabelPlacement: 'inside',
+            dataPoints: [
+              { y: percentageRemaining, label: 'Total Donation' },
+              { y: donationPercentage, label: 'Your Donation' },
+            ],
+          },
         ],
-      },
-    ],
-  };
-  return (
-    <div className="p-20">
-      <CanvasJSChart options={options} />
-    </div>
-  );
+      };
+      return (
+        <div className="p-20">
+          <CanvasJSChart options={options} />
+        </div>
+      );
+    } else {
+      return (
+        <>
+          <div className="text-5xl font-bold w-full h-[55vh] mt-12 flex justify-center items-center bg-gray-100 shadow-2xl rounded-lg">
+            <h1>Please Donate First</h1>
+          </div>
+        </>
+      );
+    }
+  } else {
+    return <ErrorPage></ErrorPage>;
+  }
 };
 
 export default Statics;
